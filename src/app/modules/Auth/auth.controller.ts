65b960110b7d10b4c8d6771c1d16af9ffd1dc2dd
aminul <rahathssn@gmail.com>
2025-02-26 18:48:25 +0600

    added auth controller

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -5,23 +5,21 @@ import { AuthServices } from "./auth.service";
 import sendResponse from "../../../shared/sendResponse";
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
-  console.log("From controller:", req.body);
   const result = await AuthServices.loginUser(req.body);
 
-  // const { refreshToken } = result;
+  const { refreshToken } = result;
 
-  // res.cookie("refreshToken", refreshToken, {
-  //   secure: false,
-  //   httpOnly: true,
-  // });
+  res.cookie("refreshToken", refreshToken, {
+    secure: false,
+    httpOnly: true,
+  });
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: "Logged in successfully!",
     data: {
-      // accessToken: result.accessToken,
-      // needPasswordChange: result.needPasswordChange,
+      accessToken: result.accessToken,
     },
   });
 });
